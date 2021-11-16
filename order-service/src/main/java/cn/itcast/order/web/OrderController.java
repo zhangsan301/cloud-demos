351030db734c13bd27f53451aedfd7dca2ba024e
zhangsan_301 <3152319511@qq.com>
2021-11-16 20:10:57 +0800

    cloud-demo是服务，它里面又包含order和user两个微服务,order和user的访问端口不同，order里面调用了user里面的用户信息，它不是直接查数据库，而是采用向user服务发请求，从而获得order对应的user信息

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -3,10 +3,13 @@ package cn.itcast.order.web;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("order")
@@ -20,4 +23,6 @@ public class OrderController {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
     }
+
+
 }
